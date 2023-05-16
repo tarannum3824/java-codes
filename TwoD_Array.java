7b07164edc8b3565d3f3e9035dd7f938d622e01e
tarannum3824 <125120619+tarannum3824@users.noreply.github.com>
2023-05-16 18:19:00 +0530

    Create TwoD_Array.java

---
+++ TwoD_Array.java
@@ -0,0 +1,12 @@
+//syntax of 2D array   " int [][]a={{elements}};"
+// here we can access the index value of 2d array
+class TwoD_Array{
+    public static void main(String [] args){
+        int [][] a = {
+            {1,5,3},
+            {3,6,8},
+            {0,-1,-8}};
+            int arr = a[1][2];
+        System.out.println(arr);
+    }
+}
