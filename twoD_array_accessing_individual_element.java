f1979402de6b89faaac93dd79c187341b1b2de8e
tarannum3824 <125120619+tarannum3824@users.noreply.github.com>
2023-05-16 18:24:37 +0530

    Create twoD_array_accessing_individual_element.java

---
+++ twoD_array_accessing_individual_element.java
@@ -0,0 +1,14 @@
+class twoD_array_accessing_individual_element{
+    public static void main(String[] args){
+        int [][] a ={
+            {2,6,4},
+            {3,-5,-7}
+        };
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[0][2]);
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+        System.out.println(a[1][2]);
+    }
+}
